31b8cfba11d672925d2041c28e679d9d5559a848
Jeffrey Quesnelle <jq@jeffq.com>
2023-04-12 13:05:34 +0000

    quantize alpaca

--- backend/alpaca.py
+++ backend/alpaca.py
@@ -4,6 +4,8 @@ from celery.utils.log import get_task_logger
 app = Celery(__name__)
 logger = get_task_logger(__name__)
 
+QUANTIZE = True
+
 model = None
 tokenizer = None
 
@@ -17,22 +19,27 @@ class Bootstep(bootsteps.Step):
 
         global model, tokenizer
         model = LlamaForCausalLM.from_pretrained(
-            "chavinlo/alpaca-native", device_map="auto", torch_dtype=torch.bfloat16)
+            "chavinlo/gpt4-x-alpaca",
+            device_map="auto",
+            torch_dtype=torch.bfloat16,
+            load_in_8bit=QUANTIZE,
+        )
         logger.info(f"device map: {model.hf_device_map}")
-        tokenizer = LlamaTokenizer.from_pretrained("chavinlo/alpaca-native")
+        tokenizer = LlamaTokenizer.from_pretrained("chavinlo/gpt4-x-alpaca")
         tokenizer.model_max_length = 2048
 
 
-app.steps['worker'].add(Bootstep)
+app.steps["worker"].add(Bootstep)
 
 
 @app.task
 def alpaca(prompt: str, max_new_tokens: int = 256, new_tokens_only: bool = True) -> str:
     import torch
+
     with torch.inference_mode():
         inputs = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
         outputs = model.generate(inputs, max_new_tokens=max_new_tokens)
         output = tokenizer.decode(outputs[0], skip_special_tokens=True)
         if new_tokens_only:
-            output = output[len(prompt):]
+            output = output[len(prompt) :]
         return output
