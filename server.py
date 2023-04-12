bf5721b985632d816f19eacd2a245dac7abf4ff9
Jeffrey Quesnelle <jq@jeffq.com>
2023-04-12 03:28:55 +0000

    server changes

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 import logging
+import time
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from celery import group
 from backend.generate import generate
 from backend.alpaca import alpaca
+from backend.tts import tts
+from backend.general import combine_audio_convert_and_upload, get_existing_audio_for_text
+from utils import split_and_recombine_text, text_hash
 
-SUMMARY_PROMPT = "TEXT BEFORE EXCERPT:\n{text_before_excerpt}\n\nEXCERPT:\n{excerpt}\n\nSHORT SUMMARY OF ONLY THE EXCERPT:\n"
+# SUMMARY_PROMPT = "TEXT BEFORE EXCERPT:\n{text_before_excerpt}\n\nEXCERPT:\n{excerpt}\n\nSHORT SUMMARY OF ONLY THE EXCERPT:\n"
+# SUMMARY_PROMPT = "Write a short summary of the following text.\n\n{text_before_excerpt} {excerpt}\n\nSHORT SUMMARY:\n"
+SUMMARY_PROMPT = "Below is an EXCERPT from a book, and the TEXT IMMEDIATELY BEFORE EXCERPT. In the active voice, write an interesting SHORT SUMMARY OF ONLY EXCERPT, using TEXT IMMEDIATELY BEFORE EXCERPT to provide context, but not directly summarizing TEXT IMMEDIATELY BEFORE EXCERPT. Do not use the word \"excerpt\".\n\nTEXT IMMEDIATELY BEFORE EXCERPT:\n{text_before_excerpt}\n\nEXCERPT:\n{excerpt}\n\nSHORT SUMMARY OF ONLY EXCERPT:\n"
+OPTIMAL_TTS_SPLIT = 8
+MAX_TTS_SPLIT_FLACTOR = 1.25
 
 app = Flask("server")
-CORS(app, origins=["http://localhost:3000", "https://cyoa.hooloovoo.ai"])
+CORS(app, origins=["http://localhost",
+     "http://localhost:3000", "https://cyoa.hooloovoo.ai"])
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
@@ -18,30 +27,50 @@ if __name__ != '__main__':
 
 @app.route("/generate", methods=["POST"])
 async def generate_api():
-    if request.method != "POST":
-        abort(404)
-
     args = request.json
     summarize = args.pop("summarize", True)
     generations = args.get("generations", 1)
     args["generations"] = 1
 
-    app.logger.info("Starting generations...")
     results = group(generate.signature((), args)
                     for _ in range(0, generations)).apply_async().get()
     results = [result[0] for result in results]
 
     if summarize:
-        text_before_excerpt = args.get("text", "")[-1000:]
+        text_before_excerpt = args.get("text", "")[-500:]
         prompts = [SUMMARY_PROMPT.format(
             text_before_excerpt=text_before_excerpt, excerpt=result) for result in results]
 
-        app.logger.info("Starting summaries...")
         summaries = group([alpaca.s(prompt)
                           for prompt in prompts]).apply_async().get()
     else:
         summaries = []
 
-    app.logger.info("Finished")
-
     return jsonify({"results": results, "summaries": summaries})
+
+
+@app.route("/imagine", methods=["POST"])
+async def imagine_api():
+    args = request.json
+
+    text = args.get("text", "")
+    if len(text) == 0:
+        abort(500)
+    
+    hash_of_text = text_hash(text)
+
+    audio = get_existing_audio_for_text.delay(hash_of_text).get()
+    if audio["duration"] <= 0:
+        desired_length = int(len(text) / OPTIMAL_TTS_SPLIT) + 1
+        max_length = int(desired_length * MAX_TTS_SPLIT_FLACTOR) + 1
+        texts = split_and_recombine_text(text, desired_length=desired_length, max_length=max_length)
+        app.logger.info(f"Rendering audio in {len(texts)} parts using desired_length={desired_length} and max_length={max_length}")
+        start = time.time()
+        parts = group([tts.s(part) for part in texts]).apply_async().get()
+        app.logger.info(f"Rendered audio in {time.time() - start} seconds")
+        audio = combine_audio_convert_and_upload.delay(hash_of_text, parts).get()
+
+    return jsonify({
+        "audio_url": audio["url"],
+        "audio_duration": audio["duration"]
+    })
--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m gunicorn -b "0.0.0.0:443" --certfile ../fullchain.pem --keyfile ../privkey.pem server:app --timeout 600 --log-level=info
+python3 -m gunicorn -w 4 -b "0.0.0.0:443" --certfile ../fullchain.pem --keyfile ../privkey.pem server:app --timeout 600 --log-level=info
