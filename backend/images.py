661430b953109ea4aac4b49b881b0e2a3b3353b8
Jeffrey Quesnelle <jq@jeffq.com>
2023-04-16 02:51:03 +0000

    port option to images

--- backend/images.py
+++ backend/images.py
@@ -1,9 +1,12 @@
+import os
 from celery import Celery, bootsteps
 from celery.utils.log import get_task_logger
+
 
 app = Celery(__name__)
 logger = get_task_logger(__name__)
 
+port = int(os.environ["PORT"])
 
 @app.task
 def images(
@@ -11,13 +14,13 @@ def images(
     negative_prompt: str = "",
     width: int = 768,
     height: int = 512,
-    sampler_name: str = "DPM++ 2M Keras",
+    sampler_name: str = "DPM++ 2M Karras",
     steps: int = 30,
     cfg_scale: float = 10.0,
 ):
     import requests
 
-    response = requests.post("http://127.0.0.1:7861/sdapi/v1/txt2img", json={
+    response = requests.post(f"http://127.0.0.1:{port}/sdapi/v1/txt2img", json={
         "prompt": prompt,
         "negative_prompt": negative_prompt,
         "width": width,
--- worker-images.sh
+++ worker-images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A backend.images worker --loglevel=INFO --concurrency 1 --pool threads --hostname "images@%h" -Q images
+celery -A backend.images worker --loglevel=INFO --concurrency 1 --pool threads --hostname "images$PORT@%h" -Q images
